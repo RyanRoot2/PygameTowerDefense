ff2c99febf084712b1df4474ca5c210f36fdba42
RyanRoot2 <127535709+RyanRoot2@users.noreply.github.com>
2025-09-02 11:59:49 +0100

    noticed error in base sprite self._layer instead of self.layer

--- utils/base_sprite.py
+++ utils/base_sprite.py
@@ -7,7 +7,7 @@ class BaseSprite(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.dynamic_layering = dynamic_layering
-        self._layer = layer
+        self.layer = layer
 
     def update_position(self, new_pos):
         self.rect.topleft = new_pos
