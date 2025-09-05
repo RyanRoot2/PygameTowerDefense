cae5e8226444a702950e0f3612870bdcd5379e7d
Jacob Rogers <jacob.p.r@hotmail.com>
2025-09-05 17:52:31 +1200

    Added Reginald, press the space bar to meet him

--- src/tower_defense/main.py
+++ src/tower_defense/main.py
@@ -1,9 +1,13 @@
+import random
 import sys
 from typing import NamedTuple
 
 import pygame
+from pygame.math import Vector2
 
 from tower_defense.settings import SCREEN_HEIGHT, SCREEN_WIDTH
+from tower_defense.utils.asset_manager import Images
+from tower_defense.utils.base_sprite import BaseSprite
 
 
 class SpriteGroups(NamedTuple):
@@ -15,24 +19,58 @@ class SpriteGroups(NamedTuple):
     ui: pygame.sprite.Group = pygame.sprite.Group()
 
 
-def main():
+def make_reginald(group: pygame.sprite.Group):
+    """Silly test function to add a sprite at a random location."""
+    location = Vector2(
+        random.randint(0, SCREEN_WIDTH),
+        random.randint(0, SCREEN_HEIGHT),
+    )
+    reginald = BaseSprite(location, Images.ZOMBIE.load())
+    group.add(reginald)
+
+
+def setup() -> tuple[pygame.Surface, SpriteGroups]:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Simple Pygame Window")
+    pygame.display.set_caption("Tower Defense Game")
 
-    all_sprites = pygame.sprite.LayeredUpdates()
+    sprite_groups = SpriteGroups()
+    # all_sprites = pygame.sprite.LayeredUpdates()
 
+    return screen, sprite_groups
+
+
+def event_handler(sprite_groups: SpriteGroups):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_q, pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                case pygame.K_f:
+                    pygame.display.toggle_fullscreen()
+                case pygame.K_SPACE:
+                    make_reginald(sprite_groups.actors)
+
+
+def main_loop(screen: pygame.Surface, sprite_groups: SpriteGroups):
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        event_handler(sprite_groups)
 
-        screen.fill((100, 100, 100))  # Fill the screen with black
-        all_sprites.draw(screen)
+        screen.fill(pygame.Color("grey40"))
+        for group in sprite_groups:
+            group.draw(screen)
+        # all_sprites.draw(screen)
 
-        # Fill the screen with black
         pygame.display.flip()  # Update the display
+
+
+def main():
+    screen, sprite_groups = setup()
+    main_loop(screen, sprite_groups)
 
 
 if __name__ == "__main__":
--- src/tower_defense/utils/base_sprite.py
+++ src/tower_defense/utils/base_sprite.py
@@ -1,14 +1,12 @@
-from pathlib import Path
-
 import pygame
 
 from tower_defense.settings import BACKGROUND
 
 
 class BaseSprite(pygame.sprite.Sprite):
-    def __init__(self, pos, image_path: Path, dynamic_layering=False, layer=BACKGROUND, *groups):
+    def __init__(self, pos, image: pygame.Surface, dynamic_layering=False, layer=BACKGROUND, *groups):
         super().__init__(*groups)
-        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = image
         self.rect: pygame.Rect = self.image.get_rect(topleft=pos)
         self.dynamic_layering = dynamic_layering
         self.layer = layer
